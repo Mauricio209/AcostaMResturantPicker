0b085c2289dd7467de4585030a8b7af7ed48caf5
Mauricio Acosta <bernardoz209@icloud.com>
2022-10-17 15:01:26 -0700

    First Push For Repository

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//MAcosta
+//10-17-22
+//Today we will make a random resturant randomizer
+//
+Random rnd = new Random();
+
+string[] resturants =  {"IHop","Texas Road House","Apple Bees","Bob's Cafe","Red Lobster","Arbys","Chillys","Dennys","Olive Garden","Quiznos"};
+int index = rnd.Next(resturants.Length);
+Console.WriteLine(resturants[index]);
